c82176489b11164a2752ad3bb24ff8ec6662a83d
Max Duval <max.duval@theguardian.com>
2022-04-29 16:03:07 +0100

    feat: group sections into groups

--- deno/env.ts
+++ deno/env.ts
@@ -4,6 +4,14 @@ if (!token) throw new Error("Missing GITHUB_TOKEN");
 const path = Deno.env.get("GITHUB_EVENT_PATH");
 if (!path) throw new Error("Missing GITHUB_EVENT_PATH");
 
+Deno.run({
+	cmd: ["group", "This is my super group"],
+});
+
+Deno.run({
+	cmd: ["group", "This a weird sub-group"],
+});
+
 /**
  * https://docs.github.com/en/developers/webhooks-and-events/webhooks/webhook-events-and-payloads
  */
@@ -21,5 +29,9 @@ console.log({ payload });
 
 const issue_number = (payload.issue || payload.pull_request || payload).number;
 console.log({ issue_number });
+
+Deno.run({
+	cmd: ["groupend"],
+});
 
 if (!issue_number) throw new Error("Missing issue_number");
