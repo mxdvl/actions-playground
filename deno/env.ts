395fe34a882b92f8440680629aa5ca8b485d3cfd
Max Duval <max.duval@theguardian.com>
2022-04-29 16:22:18 +0100

    maybe like this?

--- deno/env.ts
+++ deno/env.ts
@@ -4,7 +4,13 @@ if (!token) throw new Error("Missing GITHUB_TOKEN");
 const path = Deno.env.get("GITHUB_EVENT_PATH");
 if (!path) throw new Error("Missing GITHUB_EVENT_PATH");
 
-console.log("group", "This is my super group");
+Deno.run({
+	cmd: ["ls"],
+});
+
+Deno.run({
+	cmd: ["which", "group"],
+});
 
 /**
  * https://docs.github.com/en/developers/webhooks-and-events/webhooks/webhook-events-and-payloads
@@ -25,7 +31,7 @@ const issue_number = (payload.issue || payload.pull_request || payload).number;
 console.log({ issue_number });
 
 Deno.run({
-	cmd: ["groupend"],
+	cmd: ["which", "groupend"],
 });
 
 if (!issue_number) throw new Error("Missing issue_number");
