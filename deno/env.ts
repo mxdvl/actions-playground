8a6d7523a8649b5774735581de4f80a4fac03710
Max Duval <max.duval@theguardian.com>
2022-04-30 18:49:22 +0100

    feat: group sections

--- deno/env.ts
+++ deno/env.ts
@@ -4,15 +4,7 @@ if (!token) throw new Error("Missing GITHUB_TOKEN");
 const path = Deno.env.get("GITHUB_EVENT_PATH");
 if (!path) throw new Error("Missing GITHUB_EVENT_PATH");
 
-Deno.run({
-	cmd: ["ls"],
-});
-
-Deno.run({
-	cmd: ["which", "group"],
-});
-
-console.log("::group::My very super group");
+console.log("::group::Log event payload");
 
 /**
  * https://docs.github.com/en/developers/webhooks-and-events/webhooks/webhook-events-and-payloads
@@ -29,13 +21,12 @@ const payload: {
 } = JSON.parse(Deno.readTextFileSync(path));
 console.log({ payload });
 
+console.log("::endgroup::");
+console.log("::group::Get issue number");
+
 const issue_number = (payload.issue || payload.pull_request || payload).number;
 console.log({ issue_number });
 
-console.log("::endgroup::");
-
-Deno.run({
-	cmd: ["which", "groupend"],
-});
-
 if (!issue_number) throw new Error("Missing issue_number");
+
+console.log("::endgroup::");
